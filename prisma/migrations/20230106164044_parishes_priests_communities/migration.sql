227b183b50d5e68e9211c577909fc0a8aecc6bb8
Ian Torquato <iantorquato2@gmail.com>
2023-01-06 13:44:58 -0300

    Renamed the database tables

--- prisma/migrations/20230106164044_parishes_priests_communities/migration.sql
+++ prisma/migrations/20230106164044_parishes_priests_communities/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Parish" (
+CREATE TABLE "parishes" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "location" TEXT NOT NULL,
@@ -7,35 +7,35 @@ CREATE TABLE "Parish" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Parish_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "parishes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Priest" (
+CREATE TABLE "priests" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Priest_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "priests_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Community" (
+CREATE TABLE "communities" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "parishId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Community_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "communities_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Parish_priestId_key" ON "Parish"("priestId");
+CREATE UNIQUE INDEX "parishes_priestId_key" ON "parishes"("priestId");
 
 -- AddForeignKey
-ALTER TABLE "Parish" ADD CONSTRAINT "Parish_priestId_fkey" FOREIGN KEY ("priestId") REFERENCES "Priest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "parishes" ADD CONSTRAINT "parishes_priestId_fkey" FOREIGN KEY ("priestId") REFERENCES "priests"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Community" ADD CONSTRAINT "Community_parishId_fkey" FOREIGN KEY ("parishId") REFERENCES "Parish"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "communities" ADD CONSTRAINT "communities_parishId_fkey" FOREIGN KEY ("parishId") REFERENCES "parishes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
